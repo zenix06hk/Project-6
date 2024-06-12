0cf82a2ddb0b038e2401ed3390ec9542df93c10a
zenix06hk <alvin06hk@yahoo.com.hk>
2024-06-12 19:17:24 +0800

    update

--- backend/controller/auth.js
+++ backend/controller/auth.js
@@ -23,22 +23,28 @@ const saltRounds = 10;
 // };
 
 exports.signUp = (req, res) => {
-  bcrypt.hash(req.body.password, saltRounds);
-  const newUser = new user({
-    email: req.body.email,
-    password: req.body.password,
-  });
-  newUser
-    .save()
-    .then(() => {
-      res.status(200).json({ message: "Password encrypt" });
+  bcrypt
+    .hash(req.body.password, saltRounds)
+    .then((hash) => {
+      console.log(req.body.password);
+      console.log("hash", hash);
+      const newUser = new userModel({
+        email: req.body.email,
+        password: hash,
+      });
+
+      newUser
+        .save()
+        .then(() => {
+          res.status(200).json({ message: "Password encrypt" });
+        })
+        .catch((error) => {
+          res.status(500).json({ message: "Password failed encrypt" });
+        });
     })
     .catch((error) => {
-      res.status(500).json({ message: "Password failed encrypt" });
+      console.log(error.message);
     });
-  res.send({
-    message: " This is point to signup POST",
-  });
 };
 
 exports.login = (req, res) => {
