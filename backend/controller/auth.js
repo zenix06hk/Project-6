52fd67187723b67de14d3eabe010e917e06289b6
zenix06hk <alvin06hk@yahoo.com.hk>
2024-06-19 18:55:10 +0800

    update

--- backend/controller/auth.js
+++ backend/controller/auth.js
@@ -1,5 +1,6 @@
 const userModel = require("../model/user");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const saltRounds = 10;
 // exports.testSave = (req, res) => {
 //   const userData = new userModel({
@@ -26,8 +27,8 @@ exports.signUp = (req, res) => {
   bcrypt
     .hash(req.body.password, saltRounds)
     .then((hash) => {
-      console.log(req.body.password);
-      console.log("hash", hash);
+      // console.log(req.body.password);
+      // console.log("hash", hash);
       const newUser = new userModel({
         email: req.body.email,
         password: hash,
@@ -48,14 +49,40 @@ exports.signUp = (req, res) => {
 };
 
 exports.login = (req, res) => {
-  ({
-    email: req.body.email,
-    password: req.body.password,
-  });
-
-  res.send({
-    message: " This is point to login POST",
-  });
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({
+          error: new Error("User not found!"),
+        });
+      }
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((valid) => {
+          if (!valid) {
+            return res.status(401).json({
+              error: new Error("Incorrect password!"),
+            });
+          }
+          const token = jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
+            expiresIn: "24h",
+          });
+          res.status(200).json({
+            userId: user._id,
+            token: token,
+          });
+        })
+        .catch((error) => {
+          res.status(500).json({
+            error: error,
+          });
+        });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        error: error,
+      });
+    });
 };
 
 // exports.login = (req, res) => {
