d618bbe9292b0e9c9a0857797f34fa18ff7eeced
Zenix <alvin06hk@yahoo.com.hk>
2024-07-17 18:57:16 +0800

    update

--- backend/controller/sauces.js
+++ backend/controller/sauces.js
@@ -3,8 +3,18 @@ const saucesModel = require("../models/sauces");
 exports.saucesList = (req, res, next) => {
   saucesModel
     .find()
-    .then((sauce) => {
-      res.status(200).json(sauce);
+    .then((sauces) => {
+      //   const addImagePath = imgUrl.map((iamge) =>{
+
+      //   }
+      // )
+      // res.status(200).json(sauce);
+      const mappedProducts = sauces.map((sauce) => {
+        saucesModel.imageUrl =
+          req.protocol + "://" + req.get("host") + "/images/" + sauce.imageUrl;
+        return sauce;
+      });
+      res.status(200).json(mappedProducts);
     })
     .catch((error) => {
       res.status(400).json({
@@ -17,22 +27,35 @@ exports.saucesSave = (req, res, next) => {
   // if (!req.body.sauces) {
   //   return res.status(400).json({ error: "Missing sauces data" });
   // }
-  const url = req.protocol + "://" + req.get("host");
-  // req.body.sauces = JSON.parse(req.body.sacues);
+  // req.body.sauces = JSON.parse(req.body.sauces);
+  const sauceDataForm = JSON.parse(req.body.sauce);
+  const imageUrl = req.protocol + "://" + req.get("host");
+  console.log(sauceDataForm);
   const sauces = new saucesModel({
-    userId: req.body.userId,
-    name: req.body.name,
-    manufacturer: req.body.manufacturer,
-    description: req.body.description,
-    mainPepper: req.body.mainPepper,
-    imageUrl: url + "/images/" + req.file.filename,
-    heat: req.body.heat,
-    likes: req.body.heat,
-    dislikes: req.body.dislikes,
-    userLiked: req.body.userLiked,
-    userDisliked: req.body.userDisliked,
+    userId: sauceDataForm.userId,
+    name: sauceDataForm.name,
+    manufacturer: sauceDataForm.manufacturer,
+    description: sauceDataForm.description,
+    mainPepper: sauceDataForm.mainPepper,
+    imageUrl: imageUrl + "/images/" + req.file.filename,
+    heat: sauceDataForm.heat,
+    likes: sauceDataForm.likes ? sauceDataForm.likes : Number(),
+    dislikes: sauceDataForm.dislikes ? sauceDataForm.likes : Number(),
+    userLiked: sauceDataForm.userLiked,
+    userDisliked: sauceDataForm.userDisliked,
   });
+  console.log(sauces.userId);
   console.log(sauces.name);
+  console.log(sauces.manufacturer);
+  console.log(sauces.description);
+  console.log(sauces.mainPepper);
+  console.log(sauces.imageUrl);
+  console.log(sauces.heat);
+  console.log(sauces.likes);
+  console.log(sauces.dislikes);
+  console.log(sauces.userLiked);
+  console.log(sauces.userDisliked);
+
   sauces
     .save()
     .then((req, res, next) => {
